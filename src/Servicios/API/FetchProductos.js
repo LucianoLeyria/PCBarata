5b096dca2ade52c55bb143489e1effa367f2609a
luciano leyria <luciano.leyria96@gmail.com>
2024-10-01 12:47:02 -0300

    push con cambio de variable de entorno

--- src/Servicios/API/FetchProductos.js
+++ src/Servicios/API/FetchProductos.js
@@ -1,5 +1,7 @@
+const urlBack = import.meta.env.VITE_BACKURL;
+
 export const getProductosDestacados = async () => {
-  const response = await fetch("http://localhost:8000/productos/destacados");
+  const response = await fetch(urlBack + "/productos/destacados");
   console.log("Response=", response);
   const productosDestacados = await response.json();
   return productosDestacados;
@@ -7,7 +9,7 @@ export const getProductosDestacados = async () => {
 
 export const getProductos = async ({ categoria, orden, nombre }) => {
   const response = await fetch(
-    `http://localhost:8000/productos/generales?categoria=${categoria}&orden=${orden}&nombre=${nombre}`
+    `${urlBack}/productos/generales?categoria=${categoria}&orden=${orden}&nombre=${nombre}`
   );
   console.log("Response=", response);
   const productosGeneral = await response.json();
